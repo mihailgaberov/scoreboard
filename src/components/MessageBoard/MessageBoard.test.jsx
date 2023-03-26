4e019479a07cbb10aa60a5bdb4871774cd810599
Mihail Gaberov <mihail.gaberov@gmail.com>
2023-03-26 12:10:37 +0200

    :recycle: Updated tests, removed manual waits via using findByText() instead of getByText()
    ref: https://kentcdodds.com/blog/common-mistakes-with-react-testing-library#using-waitfor-to-wait-for-elements-that-can-be-queried-with-find

--- src/components/MessageBoard/MessageBoard.test.jsx
+++ src/components/MessageBoard/MessageBoard.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
 import MessageBoard from "./index";
@@ -7,10 +7,6 @@ describe('MessageBoard', () => {
     it('renders correctly messages', async () => {
         render(<MessageBoard message={'Current Games'}/>);
 
-        await waitFor(() => {
-            expect(screen.getByText(/CURRENT GAMES/i)).toBeVisible()
-        }, {
-            timeout: 3000
-        })
+        expect(await screen.findByText(/CURRENT GAMES/i)).toBeVisible()
     });
 });
